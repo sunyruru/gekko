e8f07fc2b63bf496de34ebab66000adf3b35c15d
sunyruru <sunyruru@gmail.com>
2018-03-23 19:01:34 +0900

    u

--- gekko.js
+++ gekko.js
@@ -1,9 +1,9 @@
 /*
 
-  Gekko is a Bitcoin trading bot for popular Bitcoin exchanges written 
+  Gekko is a Bitcoin trading bot for popular Bitcoin exchanges written
   in node, it features multiple trading methods using technical analysis.
 
-  If you are interested in how Gekko works, read more about Gekko's 
+  If you are interested in how Gekko works, read more about Gekko's
   architecture here:
 
   https://gekko.wizbit/docs/internals/architecture.html
@@ -12,10 +12,10 @@
 
   USE AT YOUR OWN RISK!
 
-  The author of this project is NOT responsible for any damage or loss caused 
-  by this software. There can be bugs and the bot may not perform as expected 
-  or specified. Please consider testing it first with paper trading and/or 
-  backtesting on historical data. Also look at the code to see what how 
+  The author of this project is NOT responsible for any damage or loss caused
+  by this software. There can be bugs and the bot may not perform as expected
+  or specified. Please consider testing it first with paper trading and/or
+  backtesting on historical data. Also look at the code to see what how
   it is working.
 
 */
@@ -28,7 +28,7 @@ console.log(`
   $$ |/    |$$    |   $$  $$<   $$  $$<   $$ |  $$ |
   $$ |$$$$ |$$$$$/    $$$$$  \\  $$$$$  \\  $$ |  $$ |
   $$ \\__$$ |$$ |_____ $$ |$$  \\ $$ |$$  \\ $$ \\__$$ |
-  $$    $$/ $$       |$$ | $$  |$$ | $$  |$$    $$/ 
+  $$    $$/ $$       |$$ | $$  |$$ | $$  |$$    $$/
    $$$$$$/  $$$$$$$$/ $$/   $$/ $$/   $$/  $$$$$$/
 `);
 
@@ -39,23 +39,25 @@ console.log('\tI\'m gonna make you rich, Bud Fox.', '\n\n');
 
 const dirs = util.dirs();
 
-if(util.launchUI())
+if (util.launchUI()) {
   return require(util.dirs().web + 'server');
+}
 
 const pipeline = require(dirs.core + 'pipeline');
 const config = util.getConfig();
 const mode = util.gekkoMode();
 
-if(
+if (
   config.trader.enabled &&
   !config['I understand that Gekko only automates MY OWN trading strategies']
-)
-  util.die('Do you understand what Gekko will do with your money? Read this first:\n\nhttps://github.com/askmike/gekko/issues/201');
+) {
+    util.die('Do you understand what Gekko will do with your money? Read this first:\n\nhttps://github.com/askmike/gekko/issues/201');
+  }
 
 // > Ever wonder why fund managers can't beat the S&P 500?
 // > 'Cause they're sheep, and sheep get slaughtered.
 pipeline({
   config: config,
-  mode: mode
+  mode: mode,
 });
 
